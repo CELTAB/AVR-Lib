5fe6d01b7464d956f70fa93f78f1a4ec252d630d
Augusto Lopez Dantas <aldantas@protonmail.com>
2016-01-20 14:30:20 -0200

    ADC improved

--- include/avrutil/adc.h
+++ include/avrutil/adc.h
@@ -1,11 +1,36 @@
 #include <avr/io.h>
 
-void ADC_init(uint8_t division_factor);
+#define FREE_RUNNING		0
+#define ANALOG_COMPARATOR	1
+#define EXT_IRQ0		2
+#define TMR_CNT0_MATCH_A	3
+#define TMR_CNT0_OVERFLOW	4
+#define TMR_CNT1_MATCH_B	5
+#define TMR_CNT1_OVERFLOW	6
+#define TMR_CNT1_CAPTURE_EVENT	7
+#define NORMAL			8
+
+void ADC_init();
+
+void ADC_setup(uint8_t division_factor, uint8_t trigger_source, uint8_t irq);
 
 void ADC_enable_interrupt(void);
 
+void ADC_disable_interrupt(void);
+
+void ADC_enable_trigger_source(uint8_t trigger_source);
+
+void ADC_disable_trigger_source(void);
+
+void ADC_select_prescaler(uint8_t division_factor);
+
 void ADC_select_channel(uint8_t channel);
 
+/*  Use on Normal or Free Running Modes */
 void ADC_start_conversion();
 
-void ADC_read_data(uint8_t *data);
+/* Use with interrupt */
+void ADC_read(uint8_t *data);
+
+/* Use without interrupt */
+void ADC_wait_and_read(uint8_t *data);
--- src/adc.c
+++ src/adc.c
@@ -1,9 +1,25 @@
 #include <avrutil/adc.h>
 
-void ADC_init(uint8_t division_factor)
+void ADC_init(void)
 {
 	ADMUX |= 1 << REFS0;
-	ADCSRA |= (1 << ADEN) | (7 & division_factor);
+	ADCSRA |= 1 << ADEN;
+}
+
+void ADC_setup(uint8_t division_factor, uint8_t trigger_source, uint8_t irq)
+{
+	ADC_select_prescaler(division_factor);
+
+	if(trigger_source == NORMAL) {
+		ADC_disable_trigger_source();
+	} else {
+		ADC_enable_trigger_source(trigger_source);
+	}
+	if(irq) {
+		ADC_enable_interrupt();
+	} else {
+		ADC_disable_interrupt();
+	}
 }
 
 void ADC_enable_interrupt(void)
@@ -11,18 +27,49 @@ void ADC_enable_interrupt(void)
 	ADCSRA |= 1 << ADIE;
 }
 
+void ADC_disable_interrupt(void)
+{
+	ADCSRA &= ~(1 << ADIE);
+}
+
+void ADC_enable_trigger_source(uint8_t trigger_source)
+{
+	ADCSRA |= 1 << ADATE;
+	ADCSRB |= 7 & trigger_source;
+}
+
+void ADC_disable_trigger_source()
+{
+	ADCSRA &= ~(1 << ADATE);
+}
+
+void ADC_select_prescaler(uint8_t division_factor)
+{
+	ADCSRA |= 7 & division_factor;
+}
+
 void ADC_select_channel(uint8_t channel)
 {
 	ADMUX |= 15 & channel;
 }
 
+/*  Use on Normal or Free Running Modes */
 void ADC_start_conversion(void)
 {
 	ADCSRA |=  1 << ADSC;
 }
 
-void ADC_read_data(uint8_t *data)
+/* Use with interrupt */
+void ADC_read(uint8_t *data)
 {
 	data[0] = ADCL;
 	data[1] = ADCH;
+}
+
+/* Use without interrupt */
+void ADC_wait_and_read(uint8_t *data)
+{
+	while(!(ADCSRA & (1 << ADIF)));
+	ADC_read(data);
+	ADCSRA |= 1 << ADIF;
 }
